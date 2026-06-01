28442fabaf1a16bf76f83015586e9627f5c43dfb
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-06-01 19:02:26 +0530

    improve server.js add signal headers

--- server/server.js
+++ server/server.js
@@ -1,23 +1,36 @@
 import app from "./src/app.js";
+import env from "./src/config/env.js";
 import connectDB from "./src/config/db.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-    try {
-        await connectDB();
-        
-        const server = app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
+  try {
+    await connectDB();
 
-    } catch (error) {
-        console.error("Error starting server:", error);
-        process.exit(1);
-    }
-}
+    const server = app.listen(env.PORT, () => {
+      console.log(`Server is running on port ${env.PORT}`);
+    });
+
+    // graceful shutdown ke liye signal handlers set karo, taaki server ko proper tarike se close kar sako jab SIGTERM ya SIGINT signal receive ho.
+    process.on("SIGTERM", () => {
+      console.log("SIGTERM received. Closing server...");
+      server.close(() => {
+        console.log("Server closed.");
+        process.exit(0);
+      });
+    });
+// SIGINT signal ke liye bhi same handler set karo, taaki Ctrl+C se server ko stop karne par bhi graceful shutdown ho sake.
+    process.on("SIGINT", () => {
+      console.log("SIGINT received. Closing server...");
+      server.close(() => {
+        console.log("Server closed.");
+        process.exit(0);
+      });
+    });
+
+  } catch (error) {
+    console.error("Error starting server:", error.message);
+    process.exit(1);
+  }
+};
 
 startServer();
-
