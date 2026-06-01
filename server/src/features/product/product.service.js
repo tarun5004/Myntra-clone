45549b4bac004a2cad56ab387bc929df670f14a0
Tarun Raj Gaur <tarunraj7077@gmail.com>
2026-06-02 00:13:03 +0530

    add create product service

--- server/src/features/product/product.service.js
+++ server/src/features/product/product.service.js
@@ -3,6 +3,9 @@ import Product from "./product.model.js";
 import imagekit from "../../config/imagekit.js";
 import ApiError from "../../utils/ApiError.js";
 
+
+
+
 // uploadImagesToImageKit helper function define karo, jo uploaded images ko ImageKit pe upload karega aur unke URLs return karega. is function ko product controller me use karenge jab product create ya update karenge.
 const uploadImagesToImageKit = async (files = []) => {
     if (files.length === 0) return [];
@@ -23,3 +26,29 @@ const uploadImagesToImageKit = async (files = []) => {
 // Promise = "Result abhi nahi mila, future me milega"
 
 // Promise.all = "Sabka result aane ka wait karo"
+
+
+
+
+// createProduct service function define karo, jo product creation logic handle karega. is function me product details aur uploaded images ko process karke new product create karenge.
+
+// step 1: function ko async banayo, taaki isme asynchronous operations (jaise image upload) handle kar sako.
+// accept karo ek object jisme body (product details), files (uploaded images), aur userId (product creator ka ID) ho. ye parameters controller se pass honge jab product create route hit hoga.
+
+export const createProductService  = async ({body, files, userId}) => {
+    
+    // step 2: uploaded images ko ImageKit pe upload karo using the uploadImagesToImageKit helper function, aur unke URLs ko ek variable me store karo.
+    const imageUrls = await uploadImagesToImageKit(files); // uploaded images ko ImageKit pe upload karo using the helper function, aur unke URLs ko imageUrls variable me store karo.
+
+    // step 3: product details (body) me images ke URLs aur createdBy field (userId) add karo, taaki ye information product document me save ho jaye.
+    const product = await Product.create({
+    name: body.name,
+    description: body.description,
+    price: body.price,
+    category: body.category,
+    images: imageUrls,
+    createdBy: userId,
+    });
+
+    return product; // step 4: created product document return karo, taaki controller me ise response me bhej sako.
+}
